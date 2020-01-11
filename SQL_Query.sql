9be1473fa6533c307e10b3ce54747b2feecd7baa
Jasmine Pattany <pattanyj@gmail.comcd>
2020-01-11 12:01:59 -0500

    push with filter

--- SQL_Query.sql
+++ SQL_Query.sql
@@ -2,8 +2,9 @@ SELECT * FROM debt;
 
 SELECT * FROM salary;
 
+-- JOIN TABLE AND FILTER IT SO START SALARY IS LESS THAN 40,000 and debt is greater than 20,000
 SELECT debt.university_name, debt.avg_debt_per_borrower, salary.starting_med_salary, salary.mid_career_med_salary
 FROM debt
 INNER JOIN salary
 ON debt.university_name = salary.university_name
-
+WHERE starting_med_salary < 40000 and avg_debt_per_borrower > 20000;
